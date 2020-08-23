529fe4bf3ebf760fd1fa0529b0ca9ba5b4d6b4d7
Janae Quinones <janaemassa@gmail.com>
2020-08-23 07:41:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Bacon", "Eggs", "Cheese"]
-  array << "Bagel"
 end
+array << "Bagel"
 
 def add_element_to_start_of_array(array, element)
 
