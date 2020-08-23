a7a9df3c3cd68bbebf30266db1ddff673947f41a
Janae Quinones <janaemassa@gmail.com>
2020-08-23 07:26:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  ["Mason", "Draco", "Layla"]
+  add_element_to_end_of_array.push "Mango"
 end
 
 def add_element_to_start_of_array(array, element)
